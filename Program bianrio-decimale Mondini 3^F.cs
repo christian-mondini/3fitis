09ad1f1aaef39f230503a7f418387574925704b6
christian-mondini <72445524+christian-mondini@users.noreply.github.com>
2020-11-09 19:27:22 +0100

    Create Program bianrio-decimale Mondini 3^F.cs

---
+++ Program bianrio-decimale Mondini 3^F.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Da_decimale_a_binario_Mondini_3_F
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            string b;
+            b = "";
+
+            Console.WriteLine( "Inserisci un numero decimale: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (n > 0)
+            {
+                if (n % 2 == 0)
+                    b = '0' + b;
+                else
+                    b = '1' + b;
+                n = n / 2;
+            }
+
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+        }
+    }
+}
